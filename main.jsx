85d3ca942128be4768982c743e101bc27f5345da
Raymond Lead <adasta355@gmail.com>
2024-01-21 13:08:55 +0200

    Previous file structure

--- main.jsx
+++ main.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 
-import App from './components/App'
+import App from './src/components/App'
 
 createRoot(document.getElementById('root')).render(<App />)
